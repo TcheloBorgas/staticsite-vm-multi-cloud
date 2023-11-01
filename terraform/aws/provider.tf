8670d67dbc0446da65c1f244795bdc3e2e550b27
Marcelo Henrique Borgas <marcelo.borgas@edu.fecap.br>
2023-11-01 20:04:54 -0300

    teste 1

--- terraform/aws/provider.tf
+++ terraform/aws/provider.tf
@@ -6,9 +6,9 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "staticsite-vm-multicloud-001"
+    bucket         = "staticsite-vm-multicloud-001-tchelo"
     key            = "terraform.tfstate"
-    dynamodb_table = "staticsite-vm-multicloud-001"
+    dynamodb_table = "staticsite-vm-multicloud-001-tchelo"
     region         = "us-east-1"
   }
 }
