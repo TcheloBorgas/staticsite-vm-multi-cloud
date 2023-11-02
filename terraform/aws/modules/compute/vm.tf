a4fb1172cb4d0c4c7ab26695b2ab36816cc04d54
Marcelo Henrique Borgas <marcelo.borgas@edu.fecap.br>
2023-11-01 21:36:13 -0300

    again

--- terraform/aws/modules/compute/vm.tf
+++ terraform/aws/modules/compute/vm.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "sg_public" {
     name   = "sg_public"
-    vpc_id = "${var.rede_id}"
+    vpc_id = var.rede_id
     
     egress {
         from_port   = 0
@@ -13,7 +13,7 @@ resource "aws_security_group" "sg_public" {
         from_port   = 0
         to_port     = 0
         protocol    = "-1"
-        cidr_blocks = ["${var.rede_cidr}"]
+        cidr_blocks = [var.rede_cidr]
     }
 
     ingress {
@@ -24,14 +24,26 @@ resource "aws_security_group" "sg_public" {
     }
     
     ingress {
-        description = "TCP/80 from All"
-        from_port   = 80
-        to_port     = 80
+        description = "TCP/443 from All"
+        from_port   = 443
+        to_port     = 443
         protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
-
 }
+
+data "template_file" "cloud_init" {
+    template = file("./modules/compute/init/cloud_init.sh")
+}
+
+resource "aws_instance" "instance" {
+    ami                    = var.ami
+    instance_type          = "t2.micro"
+    subnet_id              = var.subnet_id
+    vpc_security_group_ids = [aws_security_group.sg_public.id]
+    user_data              = base64encode(data.template_file.cloud_init.rendered)
+}
+
 
 data "template_file" "cloud_init" {
     template = "${file("./modules/compute/init/cloud_init.sh")}"
